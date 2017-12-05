3f60dc867e7f6eef914b2a21d350c674eda73562
VanDoorenJeroen <32894538+VanDoorenJeroen@users.noreply.github.com>
2017-12-05 16:20:50 +0100

    Update BE & NL

--- Hydra/HydraCheck.ps1
+++ Hydra/HydraCheck.ps1
@@ -8,31 +8,36 @@
 
 
 <# VARIABLES #>
+$LOCATIONS = @("BE04","NL02")
+$HOMEFOLDER = "\\BE04SFP001\home$\doorenj\Windows Scripts\Powershell-Scripts\Hydra"
+$MAILBE = @("DL_BE04_Desso_IT@tarkett.com", "Nicolas.VanDenBossche@tarkett.com", "Gunter.Geysen@Tarkett.com")
+$MAILNL = @("DL_NL02_Desso_IT@tarkett.com")
 
-$HOMEFOLDER = "\\BE04SFP001\home$\doorenj\Windows Scripts\Powershell\Hydra"
-$HYDRA = Import-Csv $HOMEFOLDER\HydraTerminalBE.csv -Delimiter ";"
-
+#$MAILBE = @("Jeroen.VanDooren@tarkett.com")
+#$MAILNL = @("Jeroen.VanDooren@tarkett.com")
 
 <# SEND MAIL #>
 
-Function SendMail {
+Function SendMail ([string[]]$MailAddresses) {
     $emailSmtpServer = "smtp-eu.intra-global.net"
     $emailFrom = "NoReply@Tarkett.com"
-    $emailTo = "Jeroen.VanDooren@Tarkett.com"
-    $emails =  @("Nicolas.VanDenBossche@tarkett.com", "Gunter.Geysen@Tarkett.com")
-    $emailcc= "Kristof.vanmol@tarkett.com"
-
+    if($MailAddresses.Count -eq 1) {
+        $emailTo = $MailAddresses[0]
+        $emailMessage = New-Object System.Net.Mail.MailMessage( $emailFrom , $emailTo )
+    }
+    else {
+        $emailTo = $MailAddresses[0]
+        $emailMessage = New-Object System.Net.Mail.MailMessage( $emailFrom , $emailTo )
+        for($i = 1; $i -lt $MailAddresses.count;$i++){
+            $emailMessage.To.Add($MailAddresses[$i])
+        }
+    }
     $style = "<style>BODY{font-family: Verdana; font-size: 10pt;}"
     $style = $style + "TABLE{border: 1px solid black; border-collapse: collapse;}"
     $style = $style + "TH{border: 1px solid black; background: #dddddd; padding: 5px; }"
     $style = $style + "TD{border: 1px solid black; padding: 5px; }"
     $style = $style + "</style>"
 
-    $emailMessage = New-Object System.Net.Mail.MailMessage( $emailFrom , $emailTo )
-    $emailMessage.cc.add($emailcc)
-    foreach ($email in $emails){
-        $emailMessage.To.Add($email)
-    }
     $emailMessage.Subject = "Hydra Check" 
     $emailPre = "This message was created on $(Get-Date) from $env:COMPUTERNAME <br/>"
     #$emailPost = "Copyright by Jeroen Van Dooren"
@@ -57,68 +62,77 @@ Returns status of the device with its location and dept
 
 #>
 
-Function CheckAllHydraStations {
-    $HYDRA | foreach {
-    $Terminal = $_.Terminal
-    $Output = ""
-    $connection = Test-Connection -Computername $terminal -Quiet -Count 1
-    if (-Not $connection) {
-        $Output = "Offline"
+Function CheckAllHydraStations ($location) {
+    switch($Location){
+        "BE04" { $HYDRA = Import-Csv $HOMEFOLDER\HydraTerminalBE.csv -Delimiter ";" }
+        "NL02" { $HYDRA = Import-Csv $HOMEFOLDER\HydraTerminalNL.csv -Delimiter ";" }
     }
-    else {
-        $FileExists = '\\' + $terminal + '\C$\ctwin\ctwin.ini'
-        $CDrive = '\\' + $terminal + '\C$'
-        try {
-            $Drive = New-PSDrive -Name "T" -PSProvider "FileSystem" -Root $CDrive -ErrorAction Stop
-            if (Test-Path $FileExists) {
-                If ((Get-Item $FileExists).Length -lt 5kb) {
-                    $Output = "CTWIN overwritten"
-                }
-                else {
-                    Try {
-                        #$error = Get-WmiObject win32_process -ComputerName $Terminal -Filter "name='tasten32.exe'" -ErrorAction SilentlyContinue
-                        $ctwin = Get-WmiObject win32_process -ComputerName $Terminal -Filter "name='ctwin.exe'" -ErrorAction Stop
-                        #if ( $error ) {
-                        #    $Output = "Tasten32 Error"
-                        #}
-                        if ( $ctwin ) {
-                            $Output = "OK"
+    $HYDRA | foreach {
+        $Terminal = $_.Terminal
+        $Output = ""
+        $connection = Test-Connection -Computername $terminal -Quiet -Count 1
+        if (-Not $connection) {
+            $Output = "Offline"
+        }
+        else {
+            $FileExists = '\\' + $terminal + '\C$\ctwin\ctwin.ini'
+            $CDrive = '\\' + $terminal + '\C$'
+            try {
+                $Drive = New-PSDrive -Name "T" -PSProvider "FileSystem" -Root $CDrive -ErrorAction Stop
+                if (Test-Path $FileExists) {
+                    If ((Get-Item $FileExists).Length -lt 5kb) {
+                        $Output = "CTWIN overwritten"
+                    }
+                    else {
+                        Try {
+                            #$error = Get-WmiObject win32_process -ComputerName $Terminal -Filter "name='tasten32.exe'" -ErrorAction SilentlyContinue
+                            $ctwin = Get-WmiObject win32_process -ComputerName $Terminal -Filter "name='ctwin.exe'" -ErrorAction Stop
+                            #if ( $error ) {
+                            #    $Output = "Tasten32 Error"
+                            #}
+                            if ( $ctwin ) {
+                                $Output = "OK"
+                            }
+                            else {
+                                $Output = "CTWIN not running"
+                            }
                         }
-                        else {
-                            $Output = "CTWIN not running"
+                        catch {
+                            $Output = "$_"
                         }
                     }
-                    catch {
-                        $Output = "$_"
-}
+                }
+                else {
+                    $Output = "No ini file"
                 }
             }
-            else {
-                $Output = "No ini file"
+            Catch [System.IO.IOException] {
+                $Output = "Not reachable."
             }
-        }
-        Catch [System.IO.IOException] {
-            $Output = "Not reachable."
-        }
-        Catch {
-            $Output = "$_"
-        }
-        Finally {
-             if (Get-PSDrive T -ErrorAction SilentlyContinue) {Remove-PSDrive T}
-        }
+            Catch {
+                $Output = "$_"
+            }
+            Finally {
+                 if (Get-PSDrive T -ErrorAction SilentlyContinue) {Remove-PSDrive T}
+            }
         
+        }
+        $Notification.Add($terminal, $Output)
     }
-    $Notification.Add($terminal, $Output)
-}
+    return $HYDRA
 }
 
 
 <#Main function#>
-
-$Notification = @{}
-CheckAllHydraStations
-$HYDRA | Foreach {
-    $_ | Add-Member -type NoteProperty -Name Notification -Value $Notification.Item($_.Terminal)
+foreach ($location in $LOCATIONS) {
+    $Notification = @{}
+    $HYDRA = CheckAllHydraStations $location
+    $HYDRA | Foreach {
+        $_ | Add-Member -type NoteProperty -Name Notification -Value $Notification.Item($_.Terminal)
+    }
+    #$HYDRA | FT
+    switch ($location) {
+        "BE04" { SendMail $MAILBE }
+        "NL02" { SendMail $MAILNL }
+    }
 }
-#$HYDRA | FT
-SendMail
